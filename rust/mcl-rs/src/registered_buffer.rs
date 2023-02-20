c8137cfccf163a660935b67021f53f95df3328b1
Ryan D. Friese <ryan.friese@pnnl.gov>
2023-02-20 13:41:37 -0800

    registered_buffer.rs docs

--- rust/mcl-rs/src/registered_buffer.rs
+++ rust/mcl-rs/src/registered_buffer.rs
@@ -81,6 +81,32 @@ struct BufferMetaData {
     alloced: Arc<AtomicBool>,
 }
 
+
+/// Represents an MCL registered buffer, which is essentially a pointer
+/// to data which exists in device Resident memory. This allows multiple
+/// tasks to use the same buffer. Further, we support creating sub-buffers
+/// of a registered buffer to alleviate some  of the overhead associated with
+/// creating new buffers.
+///
+/// #Safety
+/// Given that RegisteredBuffers can be used by multiple task simultaneously, and
+/// that accelerators are often multi-threaded we need to ensure that RegisteredBuffers
+/// are safe with respect to read and write access.
+///
+/// Internal to each RegisteredBuffer, is an "allocator" which keeps track of sub buffers
+/// that have been created so that it is not possible to simultaneously have two subuffers
+/// that overlap one other and potentially modify the overlapping contents.
+///
+/// A task will delay executing, until it is able to "allocate" its subbuffer.
+///
+/// Currently this is a  mutally exclusive allocation, regardless of if the sub buffer
+/// only requires read access vs write access. This unfortunately serializes readonly tasks
+/// using overlapping regions of the RegisteredBuffer. We are working on a better allocator
+/// to relax this restriction.
+///
+/// Finally, RegisteredBuffers are reference counted objects, and will automatically free the aquired MCL resources
+/// once the last reference is dropped.
+/// 
 pub struct RegisteredBuffer<'a> {
     data: TaskArg<'a>,
     meta: BufferMetaData,
@@ -181,14 +207,63 @@ impl<'a> RegisteredBuffer<'a> {
         self.data.flags
     }
 
+    ///Return to offset into the original RegisteredBuffer this handle starts at.
+    ///
+    /// # Examples
+    ///```no_run
+    ///     let mcl = mcl_rs::MclEnvBuilder::new().num_workers(10).initialize();
+    ///     mcl.load_prog("my_path", mcl_rs::PrgType::Src);
+    ///     let mut a = vec![0;100];
+    ///     let buf = mcl.register_buffer(mcl_rs::TaskArg::inout_slice(&mut c)
+    ///            .resident(true)
+    ///            .dynamic(true),
+    ///     );
+    ///     assert_eq!(buf.offset(),0);
+    ///     let sub_buf = buf.sub_buffer(10..20);
+    ///     assert_eq!(sub_buf.offset(),10);
+    ///```
     pub fn offset(&self) -> usize {
         self.meta.offset / self.meta.orig_type_size
     }
 
+    ///Return to len of the Registered(sub)Buffer
+    ///
+    /// # Examples
+    ///```no_run
+    ///     let mcl = mcl_rs::MclEnvBuilder::new().num_workers(10).initialize();
+    ///     mcl.load_prog("my_path", mcl_rs::PrgType::Src);
+    ///     let mut a = vec![0;100];
+    ///     let buf = mcl.register_buffer(mcl_rs::TaskArg::inout_slice(&mut c)
+    ///            .resident(true)
+    ///            .dynamic(true),
+    ///     );
+    ///     assert_eq!(buf.len(),100);
+    ///     let sub_buf = buf.sub_buffer(10..20);
+    ///     assert_eq!(sub_buf.len(),10);
+    ///```
     pub fn len(&self) -> usize {
         self.meta.len / self.meta.orig_type_size
     }
 
+    /// Tries to unregistered a previously registered buffer. 
+    /// This will only succeed if this is the last reference to the registered buffer.
+    ///
+    /// # Examples
+    ///```no_run
+    ///     let mcl = mcl_rs::MclEnvBuilder::new().num_workers(10).initialize();
+    ///     mcl.load_prog("my_path", mcl_rs::PrgType::Src);
+    ///     let mut a = vec![0;100];
+    ///     let buf = mcl.register_buffer(mcl_rs::TaskArg::inout_slice(&mut c)
+    ///            .resident(true)
+    ///            .dynamic(true),
+    ///     );
+    ///     let sub_buf = buf.sub_buffer(10..20);
+    ///     let buf = buf.try_unregister();
+    ///     assert!(buf.is_err());
+    ///     let buf = buf.unwrap_err();
+    ///     drop(sub_buf);
+    ///     assert!(buf.try_unregister().is_ok())
+    ///```
     pub fn try_unregister(self) -> Result<(), Self> {
         if let Ok(_) = self
             .meta
@@ -205,6 +280,24 @@ impl<'a> RegisteredBuffer<'a> {
         }
     }
 
+    /// Tries to invalidate a previously registered buffer. 
+    /// meaning that the data on the host has changed and needs to be recopied to the device (generally when the buffer is used for input);
+    /// This will only succeed if this is the last reference to the registered buffer.
+    ///
+    /// # Examples
+    ///```no_run
+    ///     let mcl = mcl_rs::MclEnvBuilder::new().num_workers(10).initialize();
+    ///     mcl.load_prog("my_path", mcl_rs::PrgType::Src);
+    ///     let mut a = vec![0;100];
+    ///     let buf = mcl.register_buffer(mcl_rs::TaskArg::inout_slice(&mut c)
+    ///            .resident(true)
+    ///            .dynamic(true),
+    ///     );
+    ///     let sub_buf = buf.sub_buffer(10..20);
+    ///     assert!(buf.try_invalidate().is_err());
+    ///     drop(sub_buf);
+    ///     assert!(buf.try_invalidate().is_ok())
+    ///```
     pub fn try_invalidate(&self) -> bool {
         if self.meta.cnt.fetch_sub(1, Ordering::SeqCst) == 1 {
             match &self.data.data {
@@ -217,6 +310,49 @@ impl<'a> RegisteredBuffer<'a> {
         }
     }
 
+
+    /// Creates a sub buffer using the provided range from a given RegisteredBuffer 
+    /// The sub buffer essentially "locks" the elements in the provided range
+    /// delaying other sub buffers executing with overlapping elements until all references to this sub buffer
+    /// has been dropped.
+    /// Note that sub buffer element locking happens at task execution time rather that sub buffer handle creation.
+    /// This allows overlapping sub buffers be created and passed as arguments to different tasks, with the dependecies
+    /// being handled automatically based on the submission and execution order of the tasks
+    ///
+    /// One can also create sub buffers of sub buffers.
+    ///
+    /// # Examples
+    ///```no_run
+    ///     let mcl = mcl_rs::MclEnvBuilder::new().num_workers(10).initialize();
+    ///     mcl.load_prog("my_path", mcl_rs::PrgType::Src);
+    ///     let mut a = vec![0;100];
+    ///     let buf = mcl.register_buffer(mcl_rs::TaskArg::inout_slice(&mut c)
+    ///            .resident(true)
+    ///            .dynamic(true),
+    ///     );
+    ///     let sub_buf1 = buf.sub_buffer(10..20);
+    ///     let sub_buf2 = buf.sub_buffer(15..25); // this call will succeed even though it overlaps with sub_buf1
+    ///     let tasks = async move {
+    ///         let pes: [u64; 3] = [1, 1, 1]
+    ///         let task_1 = mcl.task("my_kernel", 1)
+    ///                 .arg_buffer(mcl_rs::TaskArg::output_slice(sub_buf1))
+    ///                 .dev(mcl_rs::DevType::CPU)
+    ///                 .exec(pes);
+    ///         let task_1 = mcl.task("my_kernel", 1)
+    ///                 .arg_buffer(mcl_rs::TaskArg::output_slice(sub_buf1))
+    ///                 .dev(mcl_rs::DevType::CPU)
+    ///                 .exec(pes);
+    ///         /// We can even create our next task sucessfully with the overlapping buffer because no actual work occurs until we call await
+    ///         let task_2 = mcl.task("my_kernel", 1)
+    ///                 .arg_buffer(mcl_rs::TaskArg::output_slice(sub_buf1))
+    ///                 .dev(mcl_rs::DevType::CPU)
+    ///                 .exec(pes);
+    ///         // drive both futures simultaneously -- based on the overlapping dependency, these task will in reality be executed serially
+    ///         // as the internal implementation will prevent both tasks from allocating the overlapping sub_buffer regions simultaneously
+    ///         futures::future::join_all([task_1,task_2]);
+    ///     futures::executor::block_on(task);
+    ///     
+    ///```
     pub fn sub_buffer(&self, range: impl RangeBounds<usize>) -> Self {
         let u8_start = match range.start_bound() {
             Bound::Included(idx) => idx * self.meta.orig_type_size,
@@ -300,6 +436,31 @@ impl Clone for SharedMemBuffer {
     }
 }
 
+
+/// Represents an MCL shared buffer, which is essentially a pointer
+/// to data which exists in shared memory. 
+/// When only the `shared_mem` feature is turned on this buffer will exist in host shared memory only.
+/// If instead the `pocl_extensions` feature is used, the the buffer will also exist in device shared memory.
+/// Note that `pocl_extensions` requires a patched version of POCL 1.8 to have been succesfully
+/// installed (please see <https://github.com/pnnl/mcl/tree/dev#using-custom-pocl-extensions> for more information).
+/// 
+/// A shared buffer allows tasks within different processes and applications to use the same buffer. 
+/// Further, we support creating sub-buffers of a shared buffers to alleviate some  of the overhead associated with
+/// creating new buffers.
+///
+/// #Safety
+/// Given that Shared Buffers can be used by multiple processes simultaenously they should
+/// always be considered inherantly unsafe, as we are currently able to provide saftey gaurantees
+/// within a single process. Please see the discussion on saftey for [RegisteredBuffers] for details
+/// on the protections offered within a single process.
+/// Given that Shared Buffers can be used by multiple tasks and processes simultaneously, and
+/// that accelerators are often multi-threaded we try to ensure that RegisteredBuffers
+/// are safe with respect to read and write access.
+///
+/// While we are unable to enforce read/write saftey guarantees across processes, the MCL library
+/// does provide reference counting of the underlying shared memory buffer, and will release the
+/// resources once all references across all proceesses have been dropped.
+/// 
 #[cfg(feature = "shared_mem")]
 impl Drop for SharedMemBuffer {
     fn drop(&mut self) {
@@ -375,18 +536,108 @@ impl SharedMemBuffer {
         self.flags
     }
 
+    ///Return the offset into the original SharedMemBuffer this handle starts at.
+    ///
+    /// # Examples
+    ///```no_run
+    ///     let mcl = mcl_rs::MclEnvBuilder::new().num_workers(10).initialize();
+    ///     mcl.load_prog("my_path", mcl_rs::PrgType::Src);
+    ///     let num_elems = 100;
+    ///     let buf = mcl.create_shared_buffer(mcl_rs::TaskArg::inout_shared::<u32>("my_buffer", num_elems));
+    ///     assert_eq!(buf.offset(),0);
+    ///     let sub_buf = buf.sub_buffer(10..20);
+    ///     assert_eq!(sub_buf.offset(),10);
+    ///```
     pub fn offset(&self) -> usize {
         self.meta.offset / self.meta.orig_type_size
     }
 
+    ///Return the len of this (sub)-SharedMemBuffer
+    ///
+    /// # Examples
+    ///```no_run
+    ///     let mcl = mcl_rs::MclEnvBuilder::new().num_workers(10).initialize();
+    ///     mcl.load_prog("my_path", mcl_rs::PrgType::Src);
+    ///     let num_elems = 100;
+    ///     let buf = mcl.create_shared_buffer(mcl_rs::TaskArg::inout_shared::<u32>("my_buffer", num_elems));
+    ///     assert_eq!(buf.len(),100);
+    ///     let sub_buf = buf.sub_buffer(10..20);
+    ///     assert_eq!(sub_buf.len(),10);
+    ///```
     pub fn len(&self) -> usize {
         self.meta.len / self.meta.orig_type_size
     }
 
-    pub fn detach(self) {
-        low_level::detach_shared_buffer(self.addr);
+
+    /// Try to manually detach from this shared memory segment (i.e. decrement the global buffer reference count), this will only succeed if this is the last reference locally
+    ///
+    /// NOTE: Dropping a handle potentially calls this automatically provided it is the last local (to this process) reference to the buffer.
+    ///
+    /// # Examples
+    ///```no_run
+    ///     let mcl = mcl_rs::MclEnvBuilder::new().num_workers(10).initialize();
+    ///     mcl.load_prog("my_path", mcl_rs::PrgType::Src);
+    ///
+    ///     let num_elems = 100;
+    ///     let buf = mcl.create_shared_buffer(mcl_rs::TaskArg::inout_shared::<u32>("my_buffer", num_elems));
+    ///     let sub_buf = buf.sub_buffer(10..20);
+    ///
+    ///     let buf = buf.try_detach();
+    ///     assert!(buf.is_err());
+    ///     let buf = buf.unwrap_err();
+    ///     drop(sub_buf);
+    ///     assert!(buf.try_detach().is_ok())
+    ///```
+    pub fn try_detach(self) -> Result<(),Self> {
+        if self.meta.cnt.fetch_sub(1, Ordering::SeqCst) == 1 {
+            low_level::detach_shared_buffer(self.addr);
+            Ok(())
+        } else {
+            Err(self)
+        }
     }
 
+    /// Creates a sub buffer using the provided range from a given SharedMemBuffer 
+    /// The sub buffer essentially "locks" the elements in the provided range 
+    /// *BUT ONLY ON THE CALLING PROCESS* (other processes will have no idea of these locked regions)
+    /// delaying other (local to this process) sub buffers from executing with overlapping elements until all references to this sub buffer
+    /// have been dropped.
+    /// Note that sub buffer element locking happens at task execution time rather that sub buffer handle creation.
+    /// This allows overlapping sub buffers be created and passed as arguments to different tasks, with the dependecies
+    /// being handled automatically based on the submission and execution order of the tasks
+    ///
+    /// One can also create sub buffers of sub buffers.
+    ///
+    /// # Examples
+    ///```no_run
+    ///     let mcl = mcl_rs::MclEnvBuilder::new().num_workers(10).initialize();
+    ///     mcl.load_prog("my_path", mcl_rs::PrgType::Src);
+    ///
+    ///     let num_elems = 100;
+    ///     let buf = mcl.create_shared_buffer(mcl_rs::TaskArg::inout_shared::<u32>("my_buffer", num_elems));
+    ///     let sub_buf1 = buf.sub_buffer(10..20);
+    ///     let sub_buf2 = buf.sub_buffer(15..25); // this call will succeed even though it overlaps with sub_buf1
+    ///     let tasks = async move {
+    ///         let pes: [u64; 3] = [1, 1, 1]
+    ///         let task_1 = mcl.task("my_kernel", 1)
+    ///                 .arg_buffer(mcl_rs::TaskArg::output_slice(sub_buf1))
+    ///                 .dev(mcl_rs::DevType::CPU)
+    ///                 .exec(pes);
+    ///         let task_1 = mcl.task("my_kernel", 1)
+    ///                 .arg_buffer(mcl_rs::TaskArg::output_slice(sub_buf1))
+    ///                 .dev(mcl_rs::DevType::CPU)
+    ///                 .exec(pes);
+    ///         /// We can even create our next task sucessfully with the overlapping buffer because no actual work occurs until we call await
+    ///         let task_2 = mcl.task("my_kernel", 1)
+    ///                 .arg_buffer(mcl_rs::TaskArg::output_slice(sub_buf1))
+    ///                 .dev(mcl_rs::DevType::CPU)
+    ///                 .exec(pes);
+    ///         // drive both futures simultaneously -- based on the overlapping dependency, these task will in reality be executed serially
+    ///         // as the internal implementation will prevent both tasks from allocating the overlapping sub_buffer regions simultaneously
+    ///         futures::future::join_all([task_1,task_2]);
+    ///     futures::executor::block_on(task);
+    ///     
+    ///```
     pub fn sub_buffer(&self, range: impl RangeBounds<usize>) -> Self {
         let u8_start = match range.start_bound() {
             Bound::Included(idx) => idx * self.meta.orig_type_size,
@@ -420,6 +671,21 @@ impl SharedMemBuffer {
         }
     }
 
+    /// Extract a T slice from this SharedMemBuffer handle.
+    ///
+    /// #Saftey
+    /// This is unsafe as we currently have no mechanism to guarantee the alignment of T with the alignment used to originally create the buffer
+    /// potentially in a different process. The user must ensure the alignment is valid otherwise behavior is undefined.
+    ///
+    /// # Examples
+    ///```no_run
+    ///     let mcl = mcl_rs::MclEnvBuilder::new().num_workers(10).initialize();
+    ///     mcl.load_prog("my_path", mcl_rs::PrgType::Src);
+    ///
+    ///     let num_elems = 100;
+    ///     let buf = mcl.attach_shared_buffer(mcl_rs::TaskArg::inout_shared::<u32>("my_buffer", num_elems));
+    ///     let sliced = unsafe { buf.as_slice::<u32>()};
+    ///```>
     pub unsafe fn as_slice<T>(&self) -> &[T] {
         assert_eq!(self.meta.len % std::mem::size_of::<T>(),0, "Leftover bytes when tryin to create slice i.e. (buffer len in bytes) % (size of T) != 0");
         std::slice::from_raw_parts(
@@ -428,6 +694,21 @@ impl SharedMemBuffer {
         )
     }
 
+    /// Extract a T slice from this SharedMemBuffer handle.
+    ///
+    /// #Saftey
+    /// This is unsafe as we currently have no mechanism to guarantee the alignment of T with the alignment used to originally create the buffer
+    /// potentially in a different process. The user must ensure the alignment is valid otherwise behavior is undefined.
+    ///
+    /// # Examples
+    ///```no_run
+    ///     let mcl = mcl_rs::MclEnvBuilder::new().num_workers(10).initialize();
+    ///     mcl.load_prog("my_path", mcl_rs::PrgType::Src);
+    ///
+    ///     let num_elems = 100;
+    ///     let buf = mcl.attach_shared_buffer(mcl_rs::TaskArg::inout_shared::<u32>("my_buffer", num_elems));
+    ///     let slice = unsafe { buf.as_mut_slice::<u32>()};
+    ///```>
     pub unsafe fn as_mut_slice<T>(&self) -> &mut [T] {
         assert_eq!(self.meta.len % std::mem::size_of::<T>(),0, "Leftover bytes when tryin to create slice i.e. (buffer len in bytes) % (size of T) != 0");
         std::slice::from_raw_parts_mut(self.addr as *mut T, self.meta.len)
