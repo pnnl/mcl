425fe48aae79e0b11a6ab18b1841ffacd0ff12fe
ryan.friese@pnnl.gov <ryan.friese@pnnl.gov>
2023-02-23 20:17:45 -0800

    fix warnings

--- rust/mcl-rs/src/low_level.rs
+++ rust/mcl-rs/src/low_level.rs
@@ -3,6 +3,9 @@ use libmcl_sys::*;
 #[cfg(any(feature = "shared_mem", feature = "pocl_extensions"))]
 use std::ffi::c_char;
 use std::ffi::{c_void, CStr, CString};
+
+#[cfg(any(feature = "shared_mem", feature = "pocl_extensions"))]
+#[allow(depricated)]
 use std::os::unix::raw::pid_t;
 use std::ptr::null_mut;
 use std::slice;
